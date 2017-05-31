6abb16b2dcfd903b229c6f4ce8bf9bb9c71f6eab
natsuki-sugimoto <natsuki.sugimoto@wwrp.gq>
2017-05-31 01:20:07 -0300

    travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO:-$(node -e 'process.stdout.write(require("./package.json").repository)')}
 
+if [ "$TRAVIS" = "true" ]
+then
+  # git need this, on Travis-CI nobody is defined
+  git config --global user.name "Travis CI" && \
+  git config --global user.email "travis@travis-ci.org"
+fi
+
 cd dist && \
 $(npm bin)/rimraf .git
 git init && \
